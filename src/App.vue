5580064a8c5968b71a5ade093d7a65670ff39399
Johan Bijlsma <johan@devmob.com>
2018-02-13 17:34:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div id="app">
+    <div class="header__container">
+      <div class="header__container--buttons"><ol>
+        <li>close button</li>
+        <li>minimize button</li>
+        <li>fullscreen button</li>
+      </ol></div>
+          <h1 class="pageheader">johan bijlsma</h1>
+    </div>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+// To load the style.scss file
+<style lang="sass">
+@import './assets/SCSS/style';
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import VueScrollClass from 'vue-scroll-class';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+Vue.use(VueScrollClass);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+});

aba2762b8f375c5f333fb39724b7db5a6dad5efd
Johan Bijlsma <johan@devmob.com>
2018-02-16 10:12:24 +0100

    making changes, adding travis for CI

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import introVue from './components/intro.vue';
   <div id="app">
     <nav class="navigation__container">
       <ul>
-        <li class="navigation__links"><router-link :to="{ name: 'intro' }">Home</router-link></li>
-        <li class="navigation__links"><router-link :to="{ name: 'work' }">Work / Portfolio</router-link></li>
-        <li class="navigation__links"><router-link :to="{ name: 'contact' }">Contact  </router-link></li>
+        <li class="navigation__container--links"><router-link :to="{ name: 'intro' }">Home</router-link></li>
+        <li class="navigation__container--links"><router-link :to="{ name: 'work' }">Work / Portfolio</router-link></li>
+        <li class="navigation__container--links"><router-link :to="{ name: 'contact' }">Contact  </router-link></li>
       </ul>
     </nav>
     <div class="header__container">

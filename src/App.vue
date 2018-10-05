3dc9f37b9ddef79caa6bebacb5aa591787981547
Johan Bijlsma <johan.bijlsma24@gmail.com>
2018-10-05 16:59:17 +0200

    added footer links + closing mobile menu

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 
+import func from './vue-temp/vue-editor-bridge';
 import introVue from './components/intro.vue';
 <template>
 
   <div id="app">
     <nav class="navigation__container">
       <form name="menu">
-        <input type="checkbox" name="" id="toggle_menu" >
+        <input type="checkbox" name="" id="toggle_menu">
         <label for="toggle_menu">Menu</label>
       <ul class="menu ">
-        <li class="navigation__container--links"><router-link :to="{ name: 'intro' }" exact>Home</router-link></li>
-        <li class="navigation__container--links"><router-link :to="{ name: 'work' }">Work / Portfolio</router-link></li>
-        <li class="navigation__container--links"><router-link :to="{ name: 'contact' }">Contact  </router-link></li>
+        <li class="navigation__container--links"><router-link :to="{ name: 'intro' }" exact> Home</router-link></li>
+        <li class="navigation__container--links"><router-link :to="{ name: 'work' }" >Work / Portfolio</router-link></li>
+        <li class="navigation__container--links"><router-link :to="{ name: 'contact' }" >Contact  </router-link></li>
       </ul>
 
       </form>
@@ -30,10 +31,30 @@ import introVue from './components/intro.vue';
 
 <script>
 
+const mobile_nav = document.querySelector('input#toggle_menu');
 
+// window.addEventListener("resize", closeMobileNav);
+// function closeMobileNav(){
+//   if (mobile_nav.checked == true){
+//         mobile_nav.checked = false
+//       console.log('closeMobileNav() is afgegaan');
+//       }
+//       else {
+//       console.log('anders niet');
+
+//       }
+// }
 export default {
-  name: 'App'
-  };
+  name: 'App',
+
+  watch: {
+    '$route' () {
+      if (mobile_nav.checked == true){
+        mobile_nav.checked = false
+      }
+    }
+  }
+};
 
 </script>
 
